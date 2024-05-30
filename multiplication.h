1a6d161710d4f5834ca64fab02fbfdd86801dfbb
Rostyslav Panchenko <159880130+astorpyacolla@users.noreply.github.com>
2024-05-30 04:22:52 +0300

    Update multiplication.h
    Створив функцію для множення, потребує 2 параметри

--- multiplication.h
+++ multiplication.h
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-void multiplication() {
+string multiplication(const string& num1, const string& num2) {
+
+    int len1 = num1.size();
+    int len2 = num2.size();
+    string result(len1 + len2, '0');
+
+    for (int i = len1 - 1; i >= 0; i--) {
+        int carry = 0;
+        for (int j = len2 - 1; j >= 0; j--) {
+            int mul = (num1[i] - '0') * (num2[j] - '0') + (result[i + j + 1] - '0') + carry;
+            carry = mul / 10;
+            result[i + j + 1] = (mul % 10) + '0';
+        }
+        result[i] += carry;
+    }
+
+    size_t pos = result.find_first_not_of('0');
+    if (pos != string::npos) {
+        return result.substr(pos);
+    }
+    return "0";
 
 }
